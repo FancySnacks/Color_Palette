18a5a7ef156e98e81672c1f0f019d77482312d38
FancySnacks <adrian.urbaniak1336@gmail.com>
2022-09-13 19:22:49 +0200

    Revert "Added random_rgb function"
    This reverts commit be7de695b56c21f1943a8c3ce7898ef79598f197.

--- helper_functions.py
+++ helper_functions.py
@@ -1,5 +1,3 @@
-import random
-
 # check if string parameter is a valid HEX value
 def is_hex_color(color: str) -> bool:
     import re
@@ -17,6 +15,3 @@ def hex_to_rgb(hex_value: str) -> str:
 
 def rgb_to_hex(rgb_value: tuple) -> str:
     return '#%02x%02x%02x' % (rgb_value[0], rgb_value[1], rgb_value[2])
-
-def random_rgb() -> tuple:
-    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
