510a75002dc8a84519b2e6af9d7a9ccec1e0be3e
FancySnacks <adrian.urbaniak1336@gmail.com>
2022-10-19 12:17:18 +0200

    Added cmyk to rgb conversion function

--- helper_functions.py
+++ helper_functions.py
@@ -32,13 +32,20 @@ def hex_to_rgb(hex_value: str) -> str:
 def rgb_to_hex(rgb: rgb_value) -> str:
     return '#%02x%02x%02x' % (rgb[0], rgb[1], rgb[2])
 
+def cmyk_to_rgb(cmyk: cmyk_value) -> rgb_value:
+    r = ceil((255 * (1 - cmyk[0]/100) * (1 - cmyk[3]/100)) + 1)
+    g = ceil((255 * (1 - cmyk[1]/100) * (1 - cmyk[3]/100))+ 1)
+    b = ceil((255 * (1 - cmyk[2]/100) * (1 - cmyk[3]/100))+ 1)
+    return (r,g,b)
+
 def random_rgb() -> rgb_value:
     return (randint(0, 255), randint(0, 255), randint(0, 255))
 
-def get_shade(rgb_color: rgb_value, scalar: float) -> cmyk_value:
-    return rgb_to_cmyk(rgb_color[0], rgb_color[1], int(rgb_color[2] * scalar))
+def get_shade(rgb_color: rgb_value, scalar: float = 1.0):
+    cmyk = rgb_to_cmyk(rgb_color[0], rgb_color[1], rgb_color[2], scalar)
+    return cmyk_to_rgb(cmyk)
 
-def rgb_to_cmyk(r: int, g: int, b: int) -> cmyk_value:
+def rgb_to_cmyk(r: int, g: int, b: int, scalar: float) -> cmyk_value:
     RGB_SCALE = 255
     CMYK_SCALE = 100
 
@@ -59,7 +66,7 @@ def rgb_to_cmyk(r: int, g: int, b: int) -> cmyk_value:
     k = min_cmy
 
     # rescale to the range [0,CMYK_SCALE]
-    return clamp_cmyk(c * CMYK_SCALE), clamp_cmyk(m * CMYK_SCALE), clamp_cmyk(y * CMYK_SCALE), clamp_cmyk(k * CMYK_SCALE)
+    return clamp_cmyk(c * CMYK_SCALE), clamp_cmyk(m * CMYK_SCALE), clamp_cmyk(y * CMYK_SCALE), clamp_cmyk((k * CMYK_SCALE) * scalar)
 
 def clamp_cmyk(value: float):
     return float(min(ceil(value), 100))
